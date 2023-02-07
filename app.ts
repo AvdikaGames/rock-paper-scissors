86f4e1f5b5e9179a25e721ab4edf5f84a1422f2a
Avdika <avdika@ukr.net>
2023-02-07 21:17:58 +0100

    fix link to app.ts file

--- app.ts
+++ app.ts
@@ -1,38 +1,37 @@
-
-const resultDisplay = document.querySelector('#result');
-const choicesDisplay = document.querySelector('#choices');
-const AIchoiceDisplay = document.querySelector('#AIChoice');
+const resultDisplay = document.querySelector('#result') as HTMLDivElement;
+const choicesDisplay = document.querySelector('#choices') as HTMLDivElement;
+const AIchoiceDisplay = document.querySelector('#AIChoice') as HTMLDivElement;
 const choices = ['rock', 'paper', 'scissors'];
 
-const handleClick = (e) => {
+const handleClick = (e: MouseEvent) => {
   let randomNum = Math.floor(Math.random() * choices.length);
-  AIchoiceDisplay.innerHTML = "Computer Choise: " + choices[randomNum];
-  getResults(e.target.innerHTML, choices[randomNum]);
+  AIchoiceDisplay!.innerHTML = "Computer Choise: " + choices[randomNum];
+  getResults((e.target! as HTMLElement).innerHTML, choices[randomNum]);
 }
 
 choices.forEach(choice => {
-  const button = document.createElement('button');
+  const button = document.createElement('button') as HTMLButtonElement;
   button.innerHTML = choice;
   button.addEventListener('click', handleClick);
-  choicesDisplay.appendChild(button);
+  choicesDisplay!.appendChild(button);
 })
 
-const getResults = (userChoice, computerChoice) => {
+const getResults = (userChoice: string, computerChoice: string) => {
   switch (userChoice + computerChoice) {
     case 'scissorspaper':
     case 'rockscissors':
     case 'paperrock':
-      resultDisplay.innerHTML = "YOU WIN!"
+      resultDisplay!.innerHTML = "YOU WIN!"
       break
     case 'paperscissors':
     case 'scissorsrock':
     case 'rockpaper':
-      resultDisplay.innerHTML = "YOU LOSE!"
+      resultDisplay!.innerHTML = "YOU LOSE!"
       break
     case 'paperpaper':
     case 'scissorsscissors':
     case 'rockrock':
-    resultDisplay.innerHTML = "ITS A DRAW!"
+    resultDisplay!.innerHTML = "ITS A DRAW!"
     break
   }
 }
